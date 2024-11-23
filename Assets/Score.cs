38580f02d712e24d4a1d34c5aa848ca507b105c0
JIPAT <jamesjirapat0526@gmail.com>
2024-11-23 15:54:38 +0700

    Demo_Playerturn

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
     DiceRoll dice;
-    [SerializeField] Text scoreText;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     private void Awake()
     {
