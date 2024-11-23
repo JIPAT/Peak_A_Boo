77a266059424ecc9078546f684aa2350fdce17d0
shuvupx <chanitajira123@gmail.com>
2024-11-24 00:49:02 +0700

    เดินได้แล้วโว้ยยย

--- Assets/DiceRoll.cs
+++ Assets/DiceRoll.cs
@@ -6,9 +6,11 @@ public class DiceRoll : MonoBehaviour
     [SerializeField] private float maxRandomForceValue, startRollingForce;
     private float forceX, forceY, forceZ;
     public int diceFaceNum;
+    Playermovement1 playermovement1;
     private void Awake()
     {
         Initialize();
+        playermovement1 = FindObjectOfType<Playermovement1>();
     }
     private void Update()
     {
@@ -17,10 +19,11 @@ public class DiceRoll : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
                 RollDice();
+                playermovement1.x = 0;
             }
         }
     }
-    private void RollDice()
+    public void RollDice()
     {
         body.isKinematic = false;
 
@@ -30,6 +33,7 @@ public class DiceRoll : MonoBehaviour
 
         body.AddForce(Vector3.up * startRollingForce);
         body.AddTorque(forceX, forceY, forceZ);
+
     }
     private void Initialize()
     {
--- Assets/FaceDetector.cs
+++ Assets/FaceDetector.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic; 
 using TMPro;
 using UnityEngine;
+
 public class FaceDetector : MonoBehaviour
 {
     DiceRoll dice;
+    Playermovement1 playermovement1;
     private void Awake()
     {
         dice = FindObjectOfType<DiceRoll>();
+        playermovement1 = FindObjectOfType<Playermovement1>();
     }
     private void OnTriggerStay(Collider other)
     {
@@ -17,7 +20,16 @@ public class FaceDetector : MonoBehaviour
             if (dice.GetComponent<Rigidbody>().velocity == Vector3.zero)
             {
                 dice.diceFaceNum = int.Parse(other.name);
+                if (playermovement1.x == 0)
+                {
+                    Debug.LogError("hello");
+                    StartCoroutine(playermovement1.MovePlayer());
+                    playermovement1.x = 1;
+
+                }
+
             }
+            
         }
     }
 }
